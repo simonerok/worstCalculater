8b20beeabe4b817ae72765601049f491e634d527
Simone <ssimone12@gmail.com>
2023-02-06 17:41:54 +0100

    calculator done men resultater bliver overskrevet

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const secondNumber = document.querySelector("#secondnumber");
 const clear = document.querySelector("#clear");
 const calc = document.querySelector("#calculate");
 let operatorSelect = document.querySelector("#operator");
+const checkbox = document.querySelector("#doround");
+const decimals = document.querySelector("#decimals");
+const resultContainer = document.querySelector("#results");
+
+setUp();
+/* SETUP */
+function setUp() {
+  calc.addEventListener("click", calculate);
+  clear.addEventListener("click", clearResults);
+}
 
 /* READING THE NUMBERS AND OPERATORS */
 operatorSelect.addEventListener("click", readOperator);
@@ -37,12 +47,11 @@ function readOperator() {
 console.log(readOperator());
 
 /* DO THE CALCULATION */
-calc.addEventListener("click", calculate);
 
 function calculate() {
   let operator = readOperator();
-  let firstVal = parseInt(firstNumber.value);
-  let secondVal = parseInt(secondNumber.value);
+  let firstVal = parseFloat(firstNumber.value);
+  let secondVal = parseFloat(secondNumber.value);
   let result;
 
   if (operator === "add") {
@@ -56,7 +65,28 @@ function calculate() {
   } else {
     /* denne skal være der så hvis der af en eller anden grund ikke kan være et resultat hvis man fx skriver # eller andet som den ikke kan regne ud */
     result = "Invalid operator";
+    writeResult(result);
+    return;
   }
 
+  /* THE ROUNDING */
+  if (checkbox.checked) {
+    let countDecimals = parseInt(decimals.value);
+    /* toFixed retunere det bestemte antal decimaler */
+    result = result.toFixed(countDecimals);
+    result = parseFloat(result);
+    console.log(result);
+  } else countDecimals = 0;
+  /* WRITING THE RESULT */
+  writeResult(result);
+}
+
+function writeResult(result) {
+  resultContainer.innerHTML = result;
   console.log(result);
+}
+/* CLEAR RESULT */
+function clearResults() {
+  resultContainer = "";
+  console.log(clear);
 }
